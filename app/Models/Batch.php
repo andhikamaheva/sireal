6caa9d93c5da0bed18a973cc8c89ef34b5fec363
Andhika Maheva Wicaksono <me@andhika.id>
2016-06-23 09:58:19 +0700

    Batch model first commit

--- app/Models/Batch.php
+++ app/Models/Batch.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Batch extends Model
 {
     //
+    use SoftDeletes;
+    protected $table      = 'batches';
+    protected $primaryKey = 'id';
+    protected $fillable   = [ 'semester_id',
+        'name',
+        'start_at',
+        'end_at',
+        'status'
+    ];
+    public    $timestamps = true;
+    protected $dates      = [ 'deleted_at' ];
+
+    public function semesters()
+    {
+        return $this->belongsTo('App\Models\Semester', 'semester_id');
+    }
 }
