98da827b7f005d97b06401b687514a6c02c854ad
Andhika Maheva Wicaksono <me@andhika.id>
2016-06-23 16:12:55 +0700

    Add Add batch_activities

--- app/Models/BatchActivity.php
+++ app/Models/BatchActivity.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BatchActivity extends Model
 {
     //
+    use SoftDeletes;
+    protected $table      = 'batch_activities';
+    protected $primaryKey = 'id';
+    protected $fillable   = [ 'batch_id',
+        'name',
+        'start_at',
+        'end_at'
+    ];
+    public    $timestamps = true;
+    protected $dates      = [ 'deleted_at' ];
+
+    public function batches()
+    {
+        return $this->belongsTo('App\Models\Batch', 'batch_id');
+    }
 }
