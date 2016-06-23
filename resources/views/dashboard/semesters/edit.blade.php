bbd2c5a964bbc34dba7071838ef38c8ed19f6465
Andhika Maheva Wicaksono <me@andhika.id>
2016-06-23 08:42:46 +0700

    Edit Semester View

--- resources/views/dashboard/semesters/edit.blade.php
+++ resources/views/dashboard/semesters/edit.blade.php
@@ -1,100 +1,146 @@
 @extends('dashboard.template.index')
 @section('css')
-<link rel="stylesheet" type="text/css" href="{{asset('assets/global/plugins/select2/select2.css')}}"/>
-	<script type="text/javascript" src="{{asset('assets/global/plugins/select2/select2.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/global/plugins/select2/select2.css')}}"/>
+    <script type="text/javascript" src="{{asset('assets/global/plugins/select2/select2.js')}}"></script>
+    <link rel="stylesheet" type="text/css"
+          href="{{asset('assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css')}}"/>
+
 @stop
 @section('content')
-	<!-- BEGIN CONTENT -->
-	<div class="page-content-wrapper">
-		<div class="page-content">
+    <!-- BEGIN CONTENT -->
+    <div class="page-content-wrapper">
+        <div class="page-content">
 
 
-			<!-- BEGIN PAGE HEADER-->
-			<h3 class="page-title">
-			{{ $pageTitle }} <small> {{$pageDesc}} </small>
-			</h3>
-			<div class="page-bar">
-				{!! Breadcrumbs::render('subjects.edit',  $subject->id) !!}
-			</div>
-			<!-- END PAGE HEADER-->
-			<!-- BEGIN PAGE CONTENT-->
-			<div class="row">
+            <!-- BEGIN PAGE HEADER-->
+            <h3 class="page-title">
+                {{ $pageTitle }}
+                <small> {{$pageDesc}} </small>
+            </h3>
+            <div class="page-bar">
+                {!! Breadcrumbs::render('semesters.edit', $semester->id) !!}
+            </div>
+            <!-- END PAGE HEADER-->
+            <!-- BEGIN PAGE CONTENT-->
+            <div class="row">
 
-				<div class="col-md-12">
-					<!-- BEGIN SAMPLE FORM PORTLET-->
-					<div class="portlet box ">
-<div class="row">
-	<div class="col-md-3">
-	</div>
-	<div class="col-md-6">
-		<div class="portlet-body form">
+                <div class="col-md-12">
+                    <!-- BEGIN SAMPLE FORM PORTLET-->
+                    <div class="portlet box ">
+                        <div class="row">
+                            <div class="col-md-3">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="portlet-body form">
 
-					<form role="form" method="post" action="{{route('subjects.update', ['id' => $subject->id])}}" style="padding-top:10px;">
-						@include('flash::message')
-						 @include('sweet::alert')
-							<input type="hidden" name="_method" value="PATCH">
-						{{csrf_field()}}
-						<div class="form-body">
-							<div class="form-group">
-								<label>Subject Code</label>
-								<div class="input-group">
+                                    <form role="form" method="post"
+                                          action="{{route('semesters.update', ['id' => $semester->id])}}"
+                                          style="padding-top:10px;">
+                                        <input type="hidden" name="_method" value="PATCH">
+                                        @include('flash::message')
+                                        @include('sweet::alert')
+                                        {{csrf_field()}}
+                                        <div class="form-body">
+                                            <div class="form-group">
+                                                <label>Semester Name</label>
+                                                <div class="input-group">
 									<span class="input-group-addon">
 									</span>
-									<input type="text" name="code" value="{{$subject->code or old('code')}}" class="form-control" placeholder="Subject Code">
+                                                    <input type="text" name="name"
+                                                           value="{{$semester->name or old('name')}}"
+                                                           class="form-control" placeholder="Semester Name">
 
-								</div>
-								For example : PBD
-							</div>
+                                                </div>
+                                                For example : Semester 16.1
+                                            </div>
 
-							<div class="form-group">
-								<label>Subject Name</label>
-								<div class="input-group">
-									<span class="input-group-addon">
-									</span>
-									<input type="text" name="name" value="{{$subject->name or old('name')}}" class="form-control" placeholder="Subject Name">
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label>Start dan End Date</label>
+                                                        <div class="input-group input-large date-picker input-daterange"
+                                                             data-date="2012/12/12" data-date-format="yyyy/mm/dd">
+                                                            <input type="text" class="form-control"
+                                                                   placeholder="Start Date" name="start_at"
+                                                                   value="{{$semester->start_at or old('start_at')}}"
+                                                                   readonly>
+												<span class="input-grokup-addon">
+												to </span>
+                                                            <input type="text" class="form-control"
+                                                                   placeholder="End Date" name="end_at"
+                                                                   value="{{$semester->end_at or old('end_at')}}"
+                                                                   readonly>
+                                                        </div>
+                                                        <!-- /input-group -->
+											<span class="help-block">
+										</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Status</label>
+                                                <div class="input-group">
 
-								</div>
-								For example : Pemrograman Basis Data
-							</div>
-						</div>
+                                                    <select class="form-control" name="status">
+                                                        <option value="1" {{$semester->status==1 ? 'active' : ''}}>
+                                                            Active
+                                                        </option>
+                                                        <option value="0" {{$semester->status==0 ? 'active' : ''}}>
+                                                            Deactive
+                                                        </option>
+                                                    </select>
+                                                </div>
 
-						<div class="form-actions" style="background-color:#FFFFFF;align:center;text-align:center;">
-							<button type="submit" class="btn blue">Submit</button>
-							<a href="{{ route('roles.index') }}"><button type="button" class="btn default">Cancel</button></a>
-						</div>
-					</form>
-				</div>
-			</div>
-			<!-- END SAMPLE FORM PORTLET-->
+                                            </div>
+                                        </div>
+                                        <div class="form-actions"
+                                             style="background-color:#FFFFFF;align:center;text-align:center;">
+                                            <button type="submit" class="btn blue">Submit</button>
+                                            <button type="button" class="btn default">Cancel</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- END SAMPLE FORM PORTLET-->
 
-		<div class="col-md-3">
-		</div>
-		</div>
-				</div>
+                            <div class="col-md-3">
+                            </div>
+                        </div>
+                    </div>
 
 
-
-</div>
-				</div>
-			</div>
-	<!-- END PAGE CONTENT-->
-		</div>
-	</div>
-	<!-- END CONTENT -->
+                </div>
+            </div>
+        </div>
+        <!-- END PAGE CONTENT-->
+    </div>
+    </div>
+    <!-- END CONTENT -->
 @stop
-	@section('js')
-		<script src="{{asset('assets/global/scripts/metronic.js')}}" type="text/javascript"></script>
-		<script src="{{asset('assets/admin/layout/scripts/layout.js')}}" type="text/javascript"></script>
-		<script src="{{asset('assets/admin/layout/scripts/quick-sidebar.js')}}" type="text/javascript"></script>
-		<script src="{{asset('assets/admin/layout/scripts/demo.js')}}" type="text/javascript"></script>
+@section('js')
 
-		<script>
-		jQuery(document).ready(function() {
-		   // initiate layout and plugins
-		Metronic.init(); // init metronic core components
-		Layout.init(); // init current layout
-		QuickSidebar.init(); // init quick sidebar
-		Demo.init(); // init demo features
-		});
-		</script>
-	@stop
+    <script type="text/javascript"
+            src="{{asset('assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js')}}"></script>
+
+
+    <script src="{{asset('assets/global/scripts/metronic.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/admin/layout/scripts/layout.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/admin/layout/scripts/quick-sidebar.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/admin/layout/scripts/demo.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/admin/pages/scripts/components-pickers.js')}}"></script>
+
+
+
+
+    <script>
+        jQuery(document).ready(function () {
+            // initiate layout and plugins
+            Metronic.init(); // init metronic core components
+            Layout.init(); // init current layout
+            Demo.init(); // init demo features
+            ComponentsPickers.init();
+        });
+    </script>
+
+
+@stop
